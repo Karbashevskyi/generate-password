34fe227f18d17e866c067f95d00917d201cb3172
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2022-10-08 17:46:23 +0200

    Add package for generate password and add index page with bootstrap CDN for build UI and add form for config password generator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Options } from 'generate-password-ts/dist/Options';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  public root(@Query() query: Options): any {
+    return this.appService.getHello(query);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { generate } from 'generate-password-ts/dist/generate';
+import { Options } from 'generate-password-ts/dist/Options';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  public defaultConfig: Options = {
+    length: 10,
+    numbers: false,
+    symbols: false,
+    uppercase: false,
+    lowercase: false,
+  };
+
+  public config: Options = {
+    length: 10,
+    numbers: true,
+    symbols: true,
+    uppercase: true,
+    lowercase: true,
+  };
+
+  public getHello(query: Options = null): {
+    message: string;
+    password: string;
+    passwordConfig: Options
+  } {
+    const passwordConfig: Options = (Object.keys(query)?.length ? {...this.defaultConfig, ...query} : null) ?? this.config;
+    return {
+      message: 'Hello world!',
+      password: this.generatePassword(query),
+      passwordConfig,
+    };
   }
+
+  public generatePassword(query: Options = null): string {
+    return generate(query ?? this.config);
+  }
+
+
 }
